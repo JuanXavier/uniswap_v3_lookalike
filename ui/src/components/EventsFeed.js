9daa01db299aed9a72899f8240cf7dc90ea368fc
Juan Xavier Valverde <juanxaviervm@hotmail.com>
2023-01-29 15:01:47 -0500

    #

--- ui/src/components/EventsFeed.js
+++ ui/src/components/EventsFeed.js
@@ -1,10 +1,10 @@
-import { ethers } from "ethers";
-import { useContext, useEffect, useReducer, useState } from "react";
-import { MetaMaskContext } from "../contexts/MetaMask";
+import { ethers } from "ethers"
+import { useContext, useEffect, useReducer, useState } from "react"
+import { MetaMaskContext } from "../contexts/MetaMask"
 
-const PoolABI = require('../abi/Pool.json');
+const PoolABI = require("../abi/Pool.json")
 
-const getEvents = (pool) => {
+const getEvents = async (pool) => {
   return Promise.all([
     pool.queryFilter("Mint", "earliest", "latest"),
     pool.queryFilter("Swap", "earliest", "latest"),
@@ -14,21 +14,22 @@ const getEvents = (pool) => {
 }
 
 const subscribeToEvents = (pool, callback) => {
-  pool.once("Mint", (a, b, c, d, e, f, g, event) => callback(event));
-  pool.once("Swap", (a, b, c, d, e, f, g, event) => callback(event));
+  pool.once("Mint", (a, b, c, d, e, f, g, event) => callback(event))
+  pool.once("Swap", (a, b, c, d, e, f, g, event) => callback(event))
 }
 
 const renderAmount = (amount) => {
-  return ethers.utils.formatUnits(amount);
+  return ethers.utils.formatUnits(amount)
 }
 
 const renderMint = (args) => {
   return (
     <span>
       <strong>Mint</strong>
-      [range: [{args.tickLower}-{args.tickUpper}], amounts: [{renderAmount(args.amount0)}, {renderAmount(args.amount1)}]]
+      [range: [{args.tickLower}-{args.tickUpper}], amounts: [{renderAmount(args.amount0)}, {renderAmount(args.amount1)}
+      ]]
     </span>
-  );
+  )
 }
 
 const renderSwap = (args) => {
@@ -37,29 +38,27 @@ const renderSwap = (args) => {
       <strong>Swap</strong>
       [amount0: {renderAmount(args.amount0)}, amount1: {renderAmount(args.amount1)}]
     </span>
-  );
-}
-
-const renderEvent = (event, i) => {
-  let content;
-
-  switch (event.event) {
-    case 'Mint':
-      content = renderMint(event.args);
-      break;
-
-    case 'Swap':
-      content = renderSwap(event.args);
-      break;
-  }
-
-  return (
-    <li key={i}>{content}</li>
   )
 }
 
+const renderEvent = (event, i) => {
+  let content
+
+  switch (event.event) {
+    case "Mint":
+      content = renderMint(event.args)
+      break
+
+    case "Swap":
+      content = renderSwap(event.args)
+      break
+  }
+
+  return <li key={i}>{content}</li>
+}
+
 const isMintOrSwap = (event) => {
-  return event.event === "Mint" || event.event === 'Swap';
+  return event.event === "Mint" || event.event === "Swap"
 }
 
 const cleanEvents = (events) => {
@@ -72,47 +71,41 @@ const cleanEvents = (events) => {
 
 const eventsReducer = (state, action) => {
   switch (action.type) {
-    case 'add':
-      return cleanEvents([action.value, ...state]);
+    case "add":
+      return cleanEvents([action.value, ...state])
 
-    case 'set':
-      return cleanEvents(action.value);
+    case "set":
+      return cleanEvents(action.value)
   }
 }
 
 const EventsFeed = (props) => {
-  const config = props.config;
-  const metamaskContext = useContext(MetaMaskContext);
-  const [events, setEvents] = useReducer(eventsReducer, []);
-  const [pool, setPool] = useState();
+  const config = props.config
+  const metamaskContext = useContext(MetaMaskContext)
+  const [events, setEvents] = useReducer(eventsReducer, [])
+  const [pool, setPool] = useState()
 
   useEffect(() => {
-    if (metamaskContext.status !== 'connected') {
-      return;
-    }
+    if (metamaskContext.status !== "connected") return
 
     if (!pool) {
       const newPool = new ethers.Contract(
         config.poolAddress,
         PoolABI,
         new ethers.providers.Web3Provider(window.ethereum)
-      );
+      )
 
-      subscribeToEvents(newPool, event => setEvents({ type: 'add', value: event }));
+      subscribeToEvents(newPool, (event) => setEvents({ type: "add", value: event }))
 
-      getEvents(newPool).then(events => {
-        setEvents({ type: 'set', value: events });
-      });
+      getEvents(newPool).then((events) => {
+        setEvents({ type: "set", value: events })
+      })
 
-      setPool(newPool);
+      setPool(newPool)
     }
-  }, [metamaskContext.status, events, pool, config]);
+  }, [metamaskContext.status, events, pool, config])
 
-  return (
-    <ul className="py-6">
-      {events.filter(isMintOrSwap).map(renderEvent)}
-    </ul>
-  );
+  return <ul className="py-6">{events.filter(isMintOrSwap).map(renderEvent)}</ul>
 }
 
-export default EventsFeed;
+export default EventsFeed
--- ui/src/components/SwapForm.js
+++ ui/src/components/SwapForm.js
@@ -1,22 +1,24 @@
-import './SwapForm.css';
-import { ethers } from 'ethers';
-import { useContext, useEffect, useState } from 'react';
-import { MetaMaskContext } from '../contexts/MetaMask';
+import "./SwapForm.css";
+import { ethers } from "ethers";
+import { useContext, useEffect, useState } from "react";
+import { MetaMaskContext } from "../contexts/MetaMask";
 
 const tokens = ["WETH", "USDC"];
 
+/* LIST OF TOKENS */
 const TokensList = (props) => {
   return (
     <select defaultValue={props.selected}>
-      {tokens.map((t) => <option key={t}>{t}</option>)}
+      {tokens.map((t) => (
+        <option key={t}>{t}</option>
+      ))}
     </select>
-  )
-}
+  );
+};
 
+/* ADD LIQUIDITY */
 const addLiquidity = (account, { token0, token1, manager }, { managerAddress, poolAddress }) => {
-  if (!token0 || !token1) {
-    return;
-  }
+  if (!token0 || !token1) return;
 
   const amount0 = ethers.utils.parseEther("0.998976618347425280");
   const amount1 = ethers.utils.parseEther("5000"); // 5000 USDC
@@ -28,63 +30,58 @@ const addLiquidity = (account, { token0, token1, manager }, { managerAddress, po
     [token0.address, token1.address, account]
   );
 
-  Promise.all(
-    [
-      token0.allowance(account, managerAddress),
-      token1.allowance(account, managerAddress)
-    ]
-  ).then(([allowance0, allowance1]) => {
-    return Promise.resolve()
-      .then(() => {
-        if (allowance0.lt(amount0)) {
-          return token0.approve(managerAddress, amount0).then(tx => tx.wait())
-        }
-      })
-      .then(() => {
-        if (allowance1.lt(amount1)) {
-          return token1.approve(managerAddress, amount1).then(tx => tx.wait())
-        }
-      })
-      .then(() => {
-        return manager.mint(poolAddress, lowerTick, upperTick, liquidity, extra)
-          .then(tx => tx.wait())
-      })
-      .then(() => {
-        alert('Liquidity added!');
-      });
-  }).catch((err) => {
-    console.error(err);
-    alert('Failed!');
-  });
-}
+  // ASYNCHRONOUS CALLS
+
+  Promise.all([token0.allowance(account, managerAddress), token1.allowance(account, managerAddress)])
+    .then(([allowance0, allowance1]) => {
+      return Promise.resolve()
+        .then(() => {
+          if (allowance0.lt(amount0)) return token0.approve(managerAddress, amount0).then((tx) => tx.wait());
+        })
+        .then(() => {
+          if (allowance1.lt(amount1)) return token1.approve(managerAddress, amount1).then((tx) => tx.wait());
+        })
+        .then(() => {
+          return manager.mint(poolAddress, lowerTick, upperTick, liquidity, extra).then((tx) => tx.wait());
+        })
+        .then(() => {
+          alert("Liquidity added!");
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      alert("Failed!");
+    });
+};
 
 const swap = (amountIn, account, { tokenIn, manager, token0, token1 }, { managerAddress, poolAddress }) => {
   const amountInWei = ethers.utils.parseEther(amountIn);
+
   const extra = ethers.utils.defaultAbiCoder.encode(
     ["address", "address", "address"],
     [token0.address, token1.address, account]
   );
 
-  tokenIn.allowance(account, managerAddress)
+  tokenIn
+    .allowance(account, managerAddress)
     .then((allowance) => {
-      if (allowance.lt(amountInWei)) {
-        return tokenIn.approve(managerAddress, amountInWei).then(tx => tx.wait())
-      }
+      if (allowance.lt(amountInWei)) return tokenIn.approve(managerAddress, amountInWei).then((tx) => tx.wait());
     })
     .then(() => {
-      return manager.swap(poolAddress, extra).then(tx => tx.wait())
+      return manager.swap(poolAddress, extra).then((tx) => tx.wait());
     })
     .then(() => {
-      alert('Swap succeeded!');
-    }).catch((err) => {
+      alert("Swap succeeded!");
+    })
+    .catch((err) => {
       console.error(err);
-      alert('Failed!');
+      alert("Failed!");
     });
-}
+};
 
 const SwapForm = (props) => {
   const metamaskContext = useContext(MetaMaskContext);
-  const enabled = metamaskContext.status === 'connected';
+  const enabled = metamaskContext.status === "connected";
 
   const amount0 = 0.008396714242162444;
   const amount1 = 42;
@@ -94,37 +91,45 @@ const SwapForm = (props) => {
   const [manager, setManager] = useState();
 
   useEffect(() => {
-    setToken0(new ethers.Contract(
-      props.config.token0Address,
-      props.config.ABIs.ERC20,
-      new ethers.providers.Web3Provider(window.ethereum).getSigner()
-    ));
-    setToken1(new ethers.Contract(
-      props.config.token1Address,
-      props.config.ABIs.ERC20,
-      new ethers.providers.Web3Provider(window.ethereum).getSigner()
-    ));
-    setManager(new ethers.Contract(
-      props.config.managerAddress,
-      props.config.ABIs.Manager,
-      new ethers.providers.Web3Provider(window.ethereum).getSigner()
-    ));
+    setToken0(
+      new ethers.Contract(
+        props.config.token0Address,
+        props.config.ABIs.ERC20,
+        new ethers.providers.Web3Provider(window.ethereum).getSigner()
+      )
+    );
+    setToken1(
+      new ethers.Contract(
+        props.config.token1Address,
+        props.config.ABIs.ERC20,
+        new ethers.providers.Web3Provider(window.ethereum).getSigner()
+      )
+    );
+    setManager(
+      new ethers.Contract(
+        props.config.managerAddress,
+        props.config.ABIs.Manager,
+        new ethers.providers.Web3Provider(window.ethereum).getSigner()
+      )
+    );
   }, []);
 
   const addLiquidity_ = () => {
     addLiquidity(metamaskContext.account, { token0, token1, manager }, props.config);
-  }
+  };
 
   const swap_ = (e) => {
     e.preventDefault();
     swap(amount1.toString(), metamaskContext.account, { tokenIn: token1, manager, token0, token1 }, props.config);
-  }
+  };
 
   return (
     <section className="SwapContainer">
       <header>
         <h1>Swap tokens</h1>
-        <button disabled={!enabled} onClick={addLiquidity_}>Add liquidity</button>
+        <button disabled={!enabled} onClick={addLiquidity_}>
+          Add liquidity
+        </button>
       </header>
       <form className="SwapForm">
         <fieldset>
@@ -135,10 +140,12 @@ const SwapForm = (props) => {
           <input type="text" placeholder="0.0" value={amount0} readOnly />
           <TokensList selected="WETH" />
         </fieldset>
-        <button disabled={!enabled} onClick={swap_}>Swap</button>
+        <button disabled={!enabled} onClick={swap_}>
+          Swap
+        </button>
       </form>
     </section>
-  )
-}
+  );
+};
 
 export default SwapForm;
